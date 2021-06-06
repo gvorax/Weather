9cd987fefbe39fee67520f7f976de5ec16f6f6c4
gvorax <70366257+gvorax@users.noreply.github.com>
2021-06-06 19:25:38 +0500

    come to root

--- src/weather/application.js
+++ src/weather/application.js
@@ -19,7 +19,7 @@ function Application() {
         setError(true);
         setQuery("");
         setTimeout(() => {
-          window.location.replace("./Weather/");
+          window.location.replace("/");
         }, 3000);
       }
     }
